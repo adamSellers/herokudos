7452dfcb6b80b53da2f8c858d6e21aad0b0fb443
Adam Sellers <adza007@gmail.com>
2017-06-24 21:36:05 +1000

    Auth working with bug around race condition in meta tags..

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  clientId: '3MVG9ZL0ppGP5UrD_6s2uriohOrG4klk1t6E9nZ7PcKoNjyeme7fwjQhKapgiiZH5tTGpG856FzCZsS2Gwekg',
+  communityURL: 'https://sdodemo-main-15355d5525b.force.com/audiophiles',
+  redirectURL: 'https://herokudos.herokuapp.com/'
 };
